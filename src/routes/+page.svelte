fd2f367885bfd96d5b6228911c1f594de50cb1c5
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 16:49:57 -0500

    add anchor to hometown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
-    import moment from "moment";
+	import moment from 'moment';
 
-    let birthDate = moment([2009, 7, 15])
+	let birthDate = moment([2009, 7, 15]);
 </script>
 
 <section id="about-me">
-    <h1>Hello!</h1>
-    <div class="paragraph-block">
-        <p>
-            You can call me Ali. I have been here on Earth for some 
-            {moment().diff(birthDate, "days")} days (or 
-            {moment().diff(birthDate, "years")} years). I was born in Cali, 
-            VAC, Colombia, and currently live in a small town nearby.
-        </p>
-        <p>
-            Making things makes me happy. Although I’m not particularly good at 
-            anything, I enjoy words, design, and fiddling with computers.
-        </p>
-        <p>
-            Hack Club is cool. You should check it out!
-        </p>
-    </div>
+	<h1>Hello!</h1>
+	<div class="paragraph-block">
+		<p>
+			You can call me Ali. I have been here on Earth for some
+			{moment().diff(birthDate, 'days')} days (or
+			{moment().diff(birthDate, 'years')} years). I was born in
+			<a href="https://www.openstreetmap.org/relation/1320707">Cali, VAC, Colombia</a>, and
+			currently live in a small town nearby.
+		</p>
+		<p>
+			Making things makes me happy. Although I’m not particularly good at anything, I enjoy words,
+			design, and fiddling with computers.
+		</p>
+		<p>Hack Club is cool. You should check it out!</p>
+	</div>
 </section>
