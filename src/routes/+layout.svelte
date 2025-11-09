482f2e81ed8668ba5aedf9c252f02457ee152e10
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 13:52:30 -0500

    add header, main and nav to +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	let { children } = $props();
-	import '$lib/styles/style.css'
+	import '$lib/styles/style.css';
 
-	let favicon 
-		= "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☀️</text></svg>"
+	let favicon =
+		'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☀️</text></svg>';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header id="header">
+	<a href="#navigation">placeholder</a>
+	<p><a href="/">hvii.cc</a></p>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<nav id="navigation">
+	<ul>
+		<li>About</li>
+		<li>Projects</li>
+		<li>Writing</li>
+	</ul>
+</nav>
