4a030798c866b465acfa5305d5500a842990c2c2
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 13:34:15 -0500

    update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
+
+	let favicon 
+		= "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☀️</text></svg>"
 </script>
 
 <svelte:head>
