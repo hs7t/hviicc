babc89104806ab7670b933ea98f15bcf4375cacd
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-10 18:57:11 -0500

    lay groundwork for Pictures section, for real

---
+++ src/routes/pictures/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	type PicturePost = {
+		date: string,
+		href: string
+	}
+
+	let posts = $derived(
+		(page.data.posts || []).map((post: any) => ({
+			date: post.meta.title,
+			href: post.path
+		} as PicturePost))
+	);
+</script>
+<section class="primary">
+    <h1>Pictures</h1>
+
+    <section>
+        
+    </section>
+</section>
+
