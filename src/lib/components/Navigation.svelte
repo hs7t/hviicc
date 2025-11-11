b8c1c9dd8f9b0998f4d24617f63c325e08483849
Alí Sinisterra <haidarconh@gmail.com>
2025-11-10 21:52:12 -0500

    update Navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -5,17 +5,21 @@
 <nav class="navigation" {id}>
 	<ul>
 		<li><a href="/">About</a></li>
-		<li><a href="/projects">Projects</a></li>
 		<li><a href="/writing">Writing</a></li>
+		<li><a href="/projects">Projects</a></li>
+		<li><a href="/pictures">Pictures</a></li>
 	</ul>
 </nav>
 
 <style>
+	nav {
+	}
 	nav ul {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 		align-items: center;
+		overflow-y: auto;
 
 		padding: 0.8rem;
 		flex-grow: 1;
