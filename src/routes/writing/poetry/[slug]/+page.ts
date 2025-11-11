623d53d22bfcb6d288d2c302b3f5c1ef2e2e954e
Alí Sinisterra <haidarconh@gmail.com>
2025-11-10 22:13:10 -0500

    fix build step, add temporary workaround

--- src/routes/writing/poetry/[slug]/+page.ts
+++ src/routes/writing/poetry/[slug]/+page.ts
@@ -1,3 +1,14 @@
+export const entries = async () => {
+	return [
+		{ slug: 'anecdote' },
+		{ slug: 'children' },
+		{ slug: 'no' },
+		{ slug: 'now' },
+		{ slug: 'spark' },
+		{ slug: 'wither' }
+	];
+};
+
 export async function load({ params }) {
 	const post = await import(`../${params.slug}.md`);
 	const { title, date } = post.metadata;
